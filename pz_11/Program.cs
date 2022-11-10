4e497f1368693021844b208670425df4fe4f3422
Арина Задорожняя <zadoroozhnyayaa@gmail.com>
2022-11-10 08:29:58 +0300

    Добавьте файлы проекта.

---
+++ pz_11/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace pz_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = "\nВсем привет" + "\nМеня возут Арина" + "\nЯ учусь на втором курсе" + "\nМне шестнадцать лет";
+
+            string[] SArr = S.Split('\n');
+            string[][] S2Arr = new string[SArr.Length][];
+
+            for (int i = 0; i < SArr.Length; i++)
+            {
+                string[] words = SArr[i].Split();
+                S2Arr[i] = words;
+
+            }
+
+        }
+    }
+}
